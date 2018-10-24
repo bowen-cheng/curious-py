875516cd1bfb609d5830774383bdde971b879953
Bowen Cheng <blueicycky@gmail.com>
2018-10-24 08:24:43 -0400

    Add demo for conditional statements and loops

--- fundamentals/basics/basics.py
+++ fundamentals/basics/basics.py
@@ -7,8 +7,8 @@ print("abs() is a built-in function: ", abs(-34), "\n")
 print("math.sqrt(4): ", m.sqrt(4))
 print("math.pow(2, 3): ", m.pow(2, 3), "\n")
 
-print("'//' - modulus, 4//3: ", 4//3)
-print("'**' - exponent, 2//3: ", 2**3, "\n")
+print("'//' - modulus, 4//3: ", 4 // 3)
+print("'**' - exponent, 2//3: ", 2 ** 3, "\n")
 
 print("0b prefix defines binary numbers, 0b10: ", 0b10)
 print("0o prefix defines octal numbers, 0o10: ", 0o10)
@@ -26,4 +26,38 @@ print("------- bool -------")
 print("bool() returns if given value is truthy or falthy")
 print("Numbers: only 0 and 0.0 is falthy", "\nbool(0):", bool(0), "\nbool(0.0):", bool(0.0))
 print("strings: only empty string is falthy", "\nbool(''):", bool(""), "\nbool('False')", bool("False"))
-print("arrays: only empty array is falthy", "\nbool([]):", bool([]), "\nbool([1, 2])", bool([1, 2]))
+print("arrays: only empty array is falthy", "\nbool([]):", bool([]), "\nbool([1, 2])", bool([1, 2]), "\n")
+
+print("------- Conditional statements -------")
+# bool(...) can be omitted in if statement:
+# if bool("eggs"):
+if "eggs":
+    print("fried eggs please")
+
+exp = ""
+if exp == "some val":
+    print("True for the 1st condition\n")
+elif exp == "some other val":
+    print("True for the 2nd condition\n")
+else:
+    print("False for other conditions\n")
+
+print("------- Loops -------")
+print("while loop:")
+counter = 6
+while counter:
+    print(counter)
+    counter -= 1
+    if counter == 2:
+        break
+
+print("\nfor loop:")
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+print("\nfor loop with index:")
+# We need the built in function enumerate() to get index in for loop
+fruits = ["apple", "banana", "cherry"]
+for index, fruit in enumerate(fruits, start=1):
+    print(index, fruit, sep=": ")
