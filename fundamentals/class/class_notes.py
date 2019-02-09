fba8b4b116957454a5384d44b2744f5d8a581d11
Bowen Cheng <blueicycky@gmail.com>
2019-02-08 21:07:00 -0500

    Use a function as a parameter in another function

--- fundamentals/class/class_notes.py
+++ fundamentals/class/class_notes.py
@@ -10,7 +10,7 @@ class MyEmptyClass:
 
 # flight = Flight() <- error, class Flight is not yet defined
 empty = MyEmptyClass()  # create a new object doesn't need new keyword like Java
-print("type(MyEmptyClass()):", type(empty), "\n")
+print("type(MyEmptyClass()):", type(empty))
 
 
 class Aircraft:
@@ -33,8 +33,8 @@ class Aircraft:
 
 
 ac = Aircraft("REG", "Bowen 777", num_rows=3, num_seats_per_row=3)
-print("ac.model():", ac.model())
-print("ac.seating_plan():", ac.seating_plan(), "\n")
+print("\nac.model():", ac.model())
+print("ac.seating_plan():", ac.seating_plan())
 
 
 class Flight:
@@ -137,20 +137,49 @@ class Flight:
             sum(1 for row_letter in row if row[row_letter] is None)  # Sum up available seats in one row
             for row in self._seating if row is not None)  # Sum up available seats in all row, skipping dummy first row
 
+    def print_boarding_cards(self, printer_func):
+        # Note that the printer function is passed in as a parameter, all functions are objects as well
+        for passenger, seat in sorted(self._passenger_seats()):
+            printer_func(passenger, self._flight_number, seat, self.aircraft_model())
+
+    def _passenger_seats(self):
+        """An iterable series of passenger and their seats"""
+        row_numbers, seat_letters = self._aircraft.seating_plan()
+        for row_number in row_numbers:
+            for seat_letter in seat_letters:
+                passenger = self._seating[row_number][seat_letter]
+                if passenger is not None:
+                    yield (passenger, "{}{}".format(row_number, seat_letter))
+
+
+def card_printer(passenger: str, flight_num: str, seat: str, aircraft: str):
+    text = "| Name: {} Flight:{} Seat: {} Aircraft: {} |".format(passenger, flight_num, seat, aircraft)
+    border = "+" + "-"*(len(text) - 2) + "+"
+    padding = "|" + " "*(len(text) - 2) + "|"
+    lines = [border, padding, text, padding, border]
+    card = "\n".join(lines)
+    print(card)
+
 
 flight = Flight("CA160", ac)
-print("flight.flight_number():", flight.flight_number())
+print("\nflight.flight_number():", flight.flight_number())
 print("flight.model():", flight.aircraft_model(), "\n")
 
 flight.allocate_seat("1A", "John")
 flight.allocate_seat("2A", "Bill")
 flight.allocate_seat("3A", "Anna")
-print("John -> 1A, Bill -> 2A, Anna -> 3A")
-pprint(flight._seating)  # All properties in classes are public in Python. Protected properties names are prefixed by _.
 # flight.allocate_seat("3A", "Kate")  # Seat occupied error
 # flight.allocate_seat("5A", "Walter")  # Invalid row number
 # flight.allocate_seat("2Z", "Walter")  # Invalid row letter
+print("John -> 1A, Bill -> 2A, Anna -> 3A")
+pprint(flight._seating)  # All properties in classes are public in Python. Protected properties names are prefixed by _.
 flight.relocate_passenger("3A", "3B")
-print("Passenger at 3A is relocated to 3B")
+
+print("\nPassenger at 3A is relocated to 3B")
 pprint(flight._seating)
-print("flight.num_available_seats()", flight.num_available_seats())
+
+print("\nflight.num_available_seats()", flight.num_available_seats())
+
+print("\nflight.print_boarding_cards(card_printer)")
+# Note that the printer function is passed in as a parameter, all functions are objects as well
+flight.print_boarding_cards(card_printer)
