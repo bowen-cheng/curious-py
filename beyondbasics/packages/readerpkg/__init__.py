3059c8dac8f7c6119c34830284af36f778d3305f
Bowen Cheng <blueicycky@gmail.com>
2019-02-23 08:52:29 -0500

    Re-arrange notes on package init file to remove warnings

--- beyondbasics/packages/readerpkg/__init__.py
+++ beyondbasics/packages/readerpkg/__init__.py
@@ -1,10 +1,4 @@
 """
-This __init__.py file tells Python the current directory is a "package"
-launch Python console in beyondbasics dir and import readerpkg, the next line will be executed
-"""
-print('Reader package is imported, oh yeah!')
-
-"""
 The import statement makes the ReaderClass accessible at the package level
 
 >>> import readerpkg
@@ -20,3 +14,9 @@ Reader package is imported
 >>> r.read()
 """
 from readerpkg.reader_module import ReaderClass
+
+"""
+This __init__.py file tells Python the current directory is a "package"
+launch Python console in beyondbasics dir and import readerpkg, the next line will be executed
+"""
+print('Reader package is imported, oh yeah!')
