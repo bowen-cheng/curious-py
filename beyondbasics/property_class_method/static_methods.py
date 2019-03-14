735d48e309f97faefbe9d4a86e41f5cb0d8d55a7
Bowen Cheng <blueicycky@gmail.com>
2019-03-13 22:43:37 -0400

    Add notes on extending static method in a child class

--- beyondbasics/property_class_method/static_methods.py
+++ beyondbasics/property_class_method/static_methods.py
@@ -8,12 +8,30 @@ class ShippingContainer:
         ShippingContainer.next_serial += 1
         return result
 
+    @staticmethod
+    def _print_info():
+        print("creating a base container...")
+
     def __init__(self, attr):
+        # ShippingContainer._print_info()  # the overridden static method will never be called
+        self._print_info()  # using self keyword allows the overridden static method to be called correctly
         self.instance_attribute = attr
         self.serial = ShippingContainer._get_next_serial()
 
 
+class SuperContainer(ShippingContainer):
+    @staticmethod
+    def _print_info():
+        """ In order to have the overridden static method called, parent class need to go through "self", not class """
+        print("creating a super container!!!")
+
+
 if __name__ == "__main__":
-    instance = ShippingContainer("val")
-    print("instance.instance_attribute:", instance.instance_attribute)
+    base_instance = ShippingContainer("base")
+    print("base_instance.instance_attribute:", base_instance.instance_attribute)
     print("ShippingContainer.next_serial:", ShippingContainer.next_serial)
+    print("\n----------\n")
+    child_instance = SuperContainer("child")
+    print("SuperContainer.next_serial:", SuperContainer.next_serial)
+    print("child_instance.instance_attribute:", child_instance.instance_attribute)
+
